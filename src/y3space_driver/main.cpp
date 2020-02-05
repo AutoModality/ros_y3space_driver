94adee4c8a862bdcb8565a84f55087ede147b831
Unknown <ajanani85@gmail.com>
2020-02-05 14:33:50 -0800

    non-ros thread publish

--- src/y3space_driver/main.cpp
+++ src/y3space_driver/main.cpp
@@ -1,10 +1,42 @@
 #include <Y3SpaceDriver.h>
+#include <thread>
+#include <memory>
+#include <signal.h>
 
+std::shared_ptr<std::thread> imu_thread;
+std::shared_ptr<Y3SpaceDriver> imu_driver;
+
+bool stop_signal;
 std::string NODE_NAME_ = "Y3SpaceDriver";
+
+//!
+//! IMU Thread Function
+//!
+void imu_thread_function()
+{
+	//Initialize
+	imu_driver->initStream();
+	while(!stop_signal)
+	{
+		imu_driver->readAndPublish();
+	}
+}
+
+
+//!
+//! Signal Handler Method
+//!
+void signal_handler(int signal)
+{
+	stop_signal = true;
+	ros::shutdown();
+	exit(0);
+}
 
 int main(int argc, char **argv)
 {
-  	ros::init(argc, argv, NODE_NAME_);
+	signal(SIGINT, signal_handler);
+  	ros::init(argc, argv, NODE_NAME_, ros::init_options::NoSigintHandler);
   	ros::NodeHandle nh;
   	ros::NodeHandle pnh("~");
 
@@ -20,6 +52,10 @@ int main(int argc, char **argv)
     pnh.param<std::string>("mode", mode, "relative");
     pnh.param<std::string>("frame", frame, "imu_link");
 
-  	Y3SpaceDriver driver(nh, pnh, port, baudrate, timeout, mode, frame);
-  	driver.run();
+  	imu_driver = std::make_shared<Y3SpaceDriver>(nh, pnh, port, baudrate, timeout, mode, frame);
+  	//driver.run();
+
+  	stop_signal = false;
+  	imu_thread = std::make_shared<std::thread>(&imu_thread_function);
+  	imu_thread->join();
 }
